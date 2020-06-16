3015e4a415f2e62164808d549d6628f99e96a90f
Abhilash Gnan <abhilashgnan@gmail.com>
2020-06-16 21:08:04 +0200

    indent using yapf

--- cli.py
+++ cli.py
@@ -6,8 +6,10 @@ import click
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 
+
 def match(pat, filename):
     return re.search(pat, filename) is not None
+
 
 def move_files(srcdir, pat, destdir):
     assert os.path.isdir(srcdir)
@@ -18,6 +20,7 @@ def move_files(srcdir, pat, destdir):
             if entry.is_file() and match(pat, entry.name):
                 final_dest = shutil.move(entry.path, destdir)
                 print(f'{entry.path} moved to {final_dest}')
+
 
 def watch_srcdir(srcdir, pat, destdir):
     event_handler = LoggingEventHandler()
@@ -33,17 +36,38 @@ def watch_srcdir(srcdir, pat, destdir):
     finally:
         observer.join()
 
+
 @click.command()
-@click.option('-s', '--source', 'source', default='.', type=click.STRING, help='Source directory path')
-@click.option('-d', '--dest-dir', required=True, type=click.STRING, help='Destination directory path')
-@click.option('-p', '--pattern', default='.*', type=click.STRING, help='Regex to match file names in source directory')
-@click.option('-w', '--watch', is_flag=True)
+@click.option(
+    '-s',
+    '--source',
+    'source',
+    default='.',
+    type=click.STRING,
+    help='Source directory path. Defaults to current directory')
+@click.option(
+    '-d',
+    '--dest-dir',
+    required=True,
+    type=click.STRING,
+    help='Destination directory path')
+@click.option(
+    '-p',
+    '--pattern',
+    default='.*',
+    type=click.STRING,
+    help='Regex to match file names in source directory. Defaults to .* (all files)')
+@click.option(
+    '-w',
+    '--watch',
+    is_flag=True,
+    help='Watch a source directory for files to move')
 def mmf(source, dest_dir, pattern, watch):
     if watch:
         watch_srcdir(source, pattern, dest_dir)
     else:
         move_files(source, pattern, dest_dir)
 
+
 if __name__ == '__main__':
     mmf()
-    
